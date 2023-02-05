24cb254f1f77fc479909cc3be7391a2bfa6a5606
Rahim Iqbal <superiqu@gmail.com>
2023-02-06 03:11:21 +0600

    Added error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,27 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import { asyncHandler } from './middlewares/async-handler';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-})
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.post('*', asyncHandler(async (req, res) => {
+  throw new NotFoundError();
+}));
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('Listening on port 3000!!!!!!!!');
 });
