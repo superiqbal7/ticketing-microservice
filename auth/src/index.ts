073f6fdf4056ae64a9ebce20ab583a59c4b47b05
Rahim Iqbal <superiqu@gmail.com>
2023-02-08 19:24:24 +0600

    Added mongodb and cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -9,7 +12,14 @@ import { NotFoundError } from './errors/not-found-error';
 import { asyncHandler } from './middlewares/async-handler';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -22,6 +32,21 @@ app.post('*', asyncHandler(async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
-});
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  }
+  catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });  
+}
+
+start();
+
