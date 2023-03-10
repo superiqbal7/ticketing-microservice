2adc965e42ed0f00987f26055556625a04d00ae8
Rahim Iqbal <superiqu@gmail.com>
2023-03-10 16:09:56 +0600

    fix jest setup for signin method type

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,27 +2,21 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      signin(): Promise<string[]>;
-    }
-  }
+  var signin: () => string[];
 }
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfasdf';
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.JWT_KEY = "asdfasdf";
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
+  const mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
 
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(mongoUri, {});
 });
 
 beforeEach(async () => {
@@ -38,11 +32,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
+global.signin = () => {
   // Build a JWT payload.  { id, email }
+  const payload = {
+    id: '1lk24j124l',
+    email: 'test@test.com',
+  };
+
   // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
   // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
   // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
   // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
   // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
 };
