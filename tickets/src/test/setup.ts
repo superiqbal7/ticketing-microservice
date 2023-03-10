9288a4bacfb5ef7b7cd10ef26111dc70ae1e78c2
Rahim Iqbal <superiqu@gmail.com>
2023-03-10 18:02:53 +0600

    add ticket service

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,8 +1,8 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
-import jwt from 'jsonwebtoken';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
   var signin: () => string[];
@@ -28,15 +28,17 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-  await mongo.stop();
+  if (mongo) {
+    await mongo.stop();
+  }
   await mongoose.connection.close();
 });
 
 global.signin = () => {
   // Build a JWT payload.  { id, email }
   const payload = {
-    id: '1lk24j124l',
-    email: 'test@test.com',
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
   };
 
   // Create the JWT!
@@ -49,7 +51,7 @@ global.signin = () => {
   const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
-  const base64 = Buffer.from(sessionJSON).toString('base64');
+  const base64 = Buffer.from(sessionJSON).toString("base64");
 
   // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
